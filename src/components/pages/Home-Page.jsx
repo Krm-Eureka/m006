46f1712ab5c1f72817d0c7fc6a3f09d507251159
Witsarut Sanongphan <witsarut_s@eurekadesign.co.th>
2024-09-10 16:37:21 +0700

    .

--- src/components/pages/Home-Page.jsx
+++ src/components/pages/Home-Page.jsx
@@ -7,43 +7,43 @@ const HomePage = () => {
         </div>
         <div className="content p-4 items-center">
           <div className=" flex flex-between flex-wrap justify-start">
-            <button className="bg-gray-400 hover:bg-gray-500 mr-2 mb-2 px-2 py-2 rounded-md block text-sm font-medium text-gray-900 dark:text-white">
+            <button className="bg-gray-400 hover:bg-gray-500 mr-2 mb-2 px-2 py-2 rounded-md block text-sm font-medium text-gray-900 hover:text-white">
             <div className="items-center px-2">
               <i className="text-xl fa-solid fa-house"></i>
               <span className="ml-2">HOME</span>
               </div>
             </button>
-            <button className="bg-gray-400 hover:bg-gray-500 mr-2 mb-2 px-2 py-2 rounded-md block text-sm font-medium text-gray-900 dark:text-white ">
+            <button className="bg-gray-400 hover:bg-gray-500 mr-2 mb-2 px-2 py-2 rounded-md block text-sm font-medium text-gray-900 hover:text-white ">
               <div className="items-center px-2">
                 <i className="text-xl fa-solid fa-square-poll-vertical"></i>
                 <span className="ml-2">Tracibility Status</span>
               </div>
             </button>
-            <button className="bg-gray-400 hover:bg-gray-500 mr-2 mb-2 px-2 py-2 rounded-md block text-sm font-medium text-gray-900 dark:text-white ">
+            <button className="bg-gray-400 hover:bg-gray-500 mr-2 mb-2 px-2 py-2 rounded-md block text-sm font-medium text-gray-900 hover:text-white ">
               <div className="items-center px-2">
                 <i className="text-xl fa-solid fa-circle-play"></i>
                 <span className="ml-2">AcousticAutoRun</span>
               </div>
             </button>
-            <button className="bg-gray-400 hover:bg-gray-500 mr-2 mb-2 px-2 py-2 rounded-md block text-sm font-medium text-gray-900 dark:text-white ">
+            <button className="bg-gray-400 hover:bg-gray-500 mr-2 mb-2 px-2 py-2 rounded-md block text-sm font-medium text-gray-900 hover:text-white ">
               <div className="items-center px-2">
                 <i className="text-xl fa-solid fa-file-contract"></i>
                 <span className="ml-2">AcousticManualRun</span>
               </div>
             </button>
-            <button className="bg-gray-400 hover:bg-gray-500 mr-2 mb-2 px-2 py-2 rounded-md block text-sm font-medium text-gray-900 dark:text-white ">
+            <button className="bg-gray-400 hover:bg-gray-500 mr-2 mb-2 px-2 py-2 rounded-md block text-sm font-medium text-gray-900 hover:text-white ">
               <div className="items-center px-2">
                 <i className="text-xl fa-solid fa-file-audio"></i>
                 <span className="ml-2">AcousticTestReport</span>
               </div>
             </button>
-            <button className="bg-gray-400 hover:bg-gray-500 mr-2 mb-2 px-2 py-2 rounded-md block text-sm font-medium text-gray-900 dark:text-white ">
+            <button className="bg-gray-400 hover:bg-gray-500 mr-2 mb-2 px-2 py-2 rounded-md block text-sm font-medium text-gray-900 hover:text-white ">
               <div className="items-center px-2">
                 <i className="text-xl fa-solid fa-folder-tree"></i>
                 <span className="ml-2">Tracibility Report</span>
               </div>
             </button>
-            <button className="bg-gray-400 hover:bg-gray-500 mr-2 mb-2 px-2 py-2 rounded-md block text-sm font-medium text-gray-900 dark:text-white ">
+            <button className="bg-gray-400 hover:bg-gray-500 mr-2 mb-2 px-2 py-2 rounded-md block text-sm font-medium text-gray-900 hover:text-white ">
               <div className="items-center px-2">
                 <i className="text-xl fa-solid fa-gears"></i>
                 <span className="ml-2">Master Setting</span>
