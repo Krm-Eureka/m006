7a864fd5ca2dd92765a96a8cb9c484822d1f6ac7
Witsarut Sanongphan <witsarut_s@eurekadesign.co.th>
2024-10-01 17:48:14 +0700

    statusBox fix

--- src/components/statusBox.jsx
+++ src/components/statusBox.jsx
@@ -1,20 +1,120 @@
 import PropTypes from "prop-types";
+import CRR from "../assets/svg/bolt-solid.svg";
+import MRK from "../assets/svg/map-pin-solid.svg";
+import MIC from "../assets/svg/microphone-solid.svg";
+import QR from "../assets/svg/qrcode-solid.svg";
+import BOX from "../assets/svg/border-all-solid.svg";
 
 const StatusBox = ({ name, status }) => {
+  const getStatusTextAndClass = (status) => {
+    switch (status) {
+  const getStatusTextAndClass = (name,status) => {
+    switch (name === 'acoustictest' ,status) {
+      case 0:
+        return {
+          text: "Exception",
+          className: "bg-yellow-500 text-white font-semibold",
+        };
+      case 1:
+        return {
+          text: "Testing",
+          className: "bg-yellow-500 text-white font-semibold",
+        };
+      case 2:
+        return {
+          text: "PASS",
+          className: "bg-green-500 text-white font-semibold",
+        };
+      case 3:
+        return {
+          text: "FAIL",
+          className: "bg-red-500 text-white font-semibold",
+        };
+      default:
+        return {
+          text: "Exception",
+          className: "bg-yellow-400 text-white font-semibold",
+        };
+    }
+    switch (name === 'lasermark' ,status) {
+      case 0:
+        return {
+          text: "Exception",
+          className: "bg-yellow-500 text-white font-semibold",
+        };
+      case 1:
+        return {
+          text: "PASS",
+          text: "Marking",
+          className: "bg-yellow-500 text-white font-semibold",
+        };
+      case 2:
+        return {
+          text: "COMPLETE",
+          className: "bg-green-500 text-white font-semibold",
+        };
+      default:
+        return {
+          text: "Exception",
+          className: "bg-yellow-400 text-white font-semibold",
+        };
+    }
+    switch (name === 'qrcode' ,status) {
+      case 0:
+        return {
+          text: "Exception",
+          className: "bg-yellow-500 text-white font-semibold",
+        };
+      case 1:
+        return {
+          text: "Reading",
+          className: "bg-yellow-500 text-white font-semibold",
+        };
+      case 2:
+        return {
+          text: "OK",
+          className: "bg-green-500 text-white font-semibold",
+        };
+        case 3:
+        return {
+          text: "FAIL",
+          className: "bg-red-500 text-white font-semibold",
+        };
+      default:
+        return {
+          text: "Exception",
+          className: "bg-yellow-400 text-white font-semibold",
+        };
+    }
+  };
+
+  const { text, className } = getStatusTextAndClass(status);
+  const icon =
+    typeof name === "string"
+      ? name.toLowerCase() === "current"
+        ? CRR
+        : name.toLowerCase() === "acoustictest"
+        ? MIC
+        : name.toLowerCase() === "qrcode"
+        ? QR
+        : name.toLowerCase() === "lasermark"
+        ? MRK
+        : name.toLowerCase() === "totalstatus"
+        ? BOX
+        : null
+      : null;
   return (
-    <div className={`status-box ${status.className}`}>
-      <p>{name}</p>
-      <p>{status.text}</p>
+    <div className="m-4 justify-start">
+      <div className={`box flex ${className} p-4 rounded-lg w-64`}>
+        <img
+          src={icon}
+          alt={name}
+          className="mr-4 h-8 w-8 justify-center mt-2"
+        />
+        <div className="flex flex-col text-center align-middle">
+          <p>{name}</p>
+          <p>{text}</p>
+        </div>
+      </div>
     </div>
   );
-};
-
-StatusBox.propTypes = {
-  name: PropTypes.string.isRequired,
-  status: PropTypes.shape({
-    text: PropTypes.string.isRequired,
-    className: PropTypes.string.isRequired,
-  }).isRequired,
-};
-
-export default StatusBox;
--- src/services/api-service/stationData.jsx
+++ src/services/api-service/stationData.jsx
@@ -1,31 +1,19 @@
 import endpoint from "../axios";
-import { setStatus } from "../setter";
+// import { setStatus } from "../setter";
 
-export async function GetLastAcousticTraceLog(version, productionLineID, SET, LOADING) {
+export async function GetLastAcousticTraceLog(
+  version,
+  productionLineID,
+  SET,
+  LOADING
+) {
   try {
     const res = await endpoint.get(
       `/api/v${version}/AcousticTraceLog/GetLastAcousticTraceLog/${productionLineID}`
     );
-    
-    console.log(`LastActTrace ${productionLineID} : `, res.data);
-    
-    const {
-      acousticStatus,
-      qrStatus,
-      laserMarkStatus,
-      tracReporJudgementtResult,
-      tracReportStatus,
-    } = res.data.data;
 
-    // Set statuses using the setStatus function
-    SET({
-      ...res.data.data,
-      acousticStatus: setStatus("acoustictest", acousticStatus),
-      qrcodeStatus: setStatus("qrcode", qrStatus),
-      lasermarkStatus: setStatus("lasermark", laserMarkStatus),
-      judgementResult: setStatus("totalstatus", tracReporJudgementtResult),
-      tracReportStatus: setStatus("totalstatus", tracReportStatus),
-    });
+    console.log(`LastActTrace ${productionLineID} : `, res.data);
+    SET(res.data.data);
 
     LOADING(!res.data.succeeded);
   } catch (error) {
@@ -33,7 +21,12 @@ export async function GetLastAcousticTraceLog(version, productionLineID, SET, LO
   }
 }
 
-export async function GetAcousticTraceDetailById(version, acousticTraceId, SET, LOADING) {
+export async function GetAcousticTraceDetailById(
+  version,
+  acousticTraceId,
+  SET,
+  LOADING
+) {
   try {
     const res = await endpoint.get(
       `/api/v${version}/AcousticTraceDetail/GetByAcousticLogId/${acousticTraceId}`
