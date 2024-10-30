6a2ea4d97e6739a30726ff81895740f0b6fbefc9
Witsarut Sanongphan <witsarut_s@eurekadesign.co.th>
2024-10-30 16:10:12 +0700

    User API Call

---
+++ src/services/api-service/userData.jsx
@@ -0,0 +1,50 @@
+import endpoint from "../axios";
+
+const getAllUsers = async () => {
+  try {
+    const url = `/api/Account/getAllUsers`;
+    const response = await endpoint.get(url);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw error;
+  }
+};
+const getUserById = async (ID) => {
+  try {
+    const url = `/api/Account/getUserById`;
+    const response = await endpoint.get(url, { id: ID });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw error;
+  }
+};
+const getAllRoles = async () => {
+  try {
+    const url = `/api/Account/getAllRoles`;
+    const response = await endpoint.get(url);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw error;
+  }
+};
+const getRoleByUserId = async (UID) => {
+  try {
+    const url = `/api/Account/getRolesByUserId`;
+    const response = await endpoint.get(url, { id: UID });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw error;
+  }
+};
+
+const userService = {
+  getAllUsers,
+  getUserById,
+  getAllRoles,
+  getRoleByUserId,
+};
+export default userService;
