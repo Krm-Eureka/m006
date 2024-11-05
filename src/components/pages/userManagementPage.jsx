20ec41c3851644a5cdf0cc55434afabbce606d82
Witsarut Sanongphan <witsarut_s@eurekadesign.co.th>
2024-11-05 15:33:27 +0700

    user manage  list data test

--- src/components/pages/userManagementPage.jsx
+++ src/components/pages/userManagementPage.jsx
@@ -220,7 +220,15 @@ const UserManagement = () => {
                       <StyledTableCell>{`${u?.firstName} ${u?.lastName}`}</StyledTableCell>
                       <StyledTableCell>{u?.roles}</StyledTableCell>
                       <StyledTableCell>
-                        {u?.isVerified ? "Verified" : "Not Verified"}
+                        {u?.isVerified === true ? (
+                          <span className="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
+                            Verified
+                          </span>
+                        ) : (
+                          <span className="bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300">
+                            Not Verified
+                          </span>
+                        )}
                       </StyledTableCell>
                       <StyledTableCell>
                         <button
