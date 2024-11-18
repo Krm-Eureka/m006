2a1fb380d63941b315f19fe1c7885f55f7329268
Witsarut Sanongphan <witsarut_s@eurekadesign.co.th>
2024-11-18 16:28:46 +0700

    .

--- src/components/pages/UnauthorizedPage.jsx
+++ src/components/pages/UnauthorizedPage.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 
 const UnauthorizedPage = () => {
-  return <div>You do not have permission to access this page. <a >HOME</a></div>;
+  return <div>You do not have permission to access this page.</div>;
 };
 
 export default UnauthorizedPage;
