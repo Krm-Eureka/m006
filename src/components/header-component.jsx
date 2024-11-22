d39ba5539bd89e2b47fcd1a9eb83966e140cd8b3
Witsarut Sanongphan <witsarut_s@eurekadesign.co.th>
2024-11-22 10:31:26 +0700

    update icon from FontAwesomeIcon

--- src/components/header-component.jsx
+++ src/components/header-component.jsx
@@ -2,8 +2,16 @@ import PropTypes from "prop-types";
 import { useState, useRef ,useEffect} from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlay, faDiagramProject, faFileContract, faFolderTree, faGears } from '@fortawesome/free-solid-svg-icons';
 import ValeoLogo from "../assets/Valeo_Logo.png";
 import OUT from "../assets/svg/logout.svg";
+import BAR from "../assets/svg/barssolid.svg";
+import AUTO from "../assets/svg/circlePlaySolid.svg";
+import RTS from "../assets/svg/fileContractSolid.svg";
+import RPT from "../assets/svg/folderTreeSolid.svg";
+import STG from "../assets/svg/gears-solid.svg";
+import QMD from "../assets/svg/diagram-project-solid.svg";
 import packageJson from '../../package.json'
 
 const HeaderLayout = (props) => {
@@ -57,7 +65,8 @@ const HeaderLayout = (props) => {
               className="text-gray-800 p-4 dark:text-gray-700"
               onClick={toggleNav}
             >
-              <i className="fa-solid fa-bars"></i>
+              {/* <i className="fa-solid fa-bars"></i> */}
+              <img className="w-5 h-5" src={BAR} alt="NavBar" />
             </button>
             <h4 className="text-gray-700 my-4 text-lg md:text-xl font-medium dark:text-gray-700">
               Stellantis OMNI Microphone Assemblyline | {PAGE}
@@ -81,7 +90,7 @@ const HeaderLayout = (props) => {
                 <img
                   src={OUT}
                   alt={"Logout"}
-                  className="mx-2 h-6 w-6 justify-center mt-1"
+                  className="mx-2 h-5 w-5 justify-center mt-1"
                 />
                 <span className="font-semibold text-lg">Logout</span>
               </div>
@@ -111,8 +120,10 @@ const HeaderLayout = (props) => {
                 to="/Console/Content_ACT/AutoRun"
                 className="bg-gray-400 hover:bg-gray-500  px-2 py-2 rounded-md block text-sm font-medium text-gray-700 hover:text-white "
               >
-                <div className="items-center px-2">
-                  <i className="text-xl fa-solid fa-circle-play"></i>
+                <div className="items-center px-2 flex">
+                  {/* <i className="text-xl fa-solid fa-circle-play"></i> */}
+                  <FontAwesomeIcon className="text-xl" icon={faCirclePlay} />
+                  {/* <img className="w-5 h-5" src={AUTO} alt="" /> */}
                   <span className="ml-2">AcousticAutoRun</span>
                 </div>
               </Link>
@@ -120,8 +131,10 @@ const HeaderLayout = (props) => {
                 to="/Console/Content_ACT/ManualRun"
                 className="bg-gray-400 hover:bg-gray-500  px-2 py-2 rounded-md block text-sm font-medium text-gray-700 hover:text-white "
               >
-                <div className="items-center px-2">
-                  <i className="text-xl fa-solid fa-file-contract"></i>
+                <div className="items-center px-2 flex">
+                  {/* <i className="text-xl fa-solid fa-file-contract"></i> */}
+                  {/* <img className="w-5 h-5" src={RTS} alt="Retest" /> */}
+                  <FontAwesomeIcon className="text-xl" icon={faFileContract} />
                   <span className="ml-2">AcousticRetest</span>
                 </div>
               </Link>
@@ -129,8 +142,10 @@ const HeaderLayout = (props) => {
                 to="/Console/Content_ACT/QMode"
                 className="bg-gray-400 hover:bg-gray-500  px-2 py-2 rounded-md block text-sm font-medium text-gray-700 hover:text-white "
               >
-                <div className="items-center px-2">
-                <i className="text-xl fa-solid fa-diagram-project"></i>
+                <div className="items-center px-2 flex">
+                {/* <i className="text-xl fa-solid fa-diagram-project"></i> */}
+                <FontAwesomeIcon className="text-xl" icon={faDiagramProject} />
+                {/* <img className="w-5 h-5" src={QMD} alt="QualityMode" /> */}
                   <span className="ml-2">Quality TestMode</span>
                 </div>
               </Link>
@@ -147,8 +162,10 @@ const HeaderLayout = (props) => {
                 to="/Console/Content_TRC/Report"
                 className="bg-gray-400 hover:bg-gray-500  px-2 py-2 rounded-md block text-sm font-medium text-gray-700 hover:text-white "
               >
-                <div className="items-center px-2">
-                  <i className="text-xl fa-solid fa-folder-tree"></i>
+                <div className="items-center px-2 flex">
+                  {/* <i className="text-xl fa-solid fa-folder-tree"></i> */}
+                  <FontAwesomeIcon className="text-xl" icon={faFolderTree} />
+                  {/* <img className="w-5 h-5" src={RPT} alt="Report" /> */}
                   <span className="ml-2">Traceability Report</span>
                 </div>
               </Link>
@@ -156,8 +173,10 @@ const HeaderLayout = (props) => {
                 to="/Console/Content_EOLT/Setting"
                 className="bg-gray-400 hover:bg-gray-500  px-2 py-2 rounded-md block text-sm font-medium text-gray-700 hover:text-white "
               >
-                <div className="items-center px-2">
-                  <i className="text-xl fa-solid fa-gears"></i>
+                <div className="items-center px-2 flex">
+                  {/* <i className="text-xl fa-solid fa-gears"></i> */}
+                  <FontAwesomeIcon className="text-xl" icon={faGears} />
+                  {/* <img className="w-5 h-5" src={STG} alt="Setting" /> */}
                   <span className="ml-2">Setting</span>
                 </div>
               </Link>
